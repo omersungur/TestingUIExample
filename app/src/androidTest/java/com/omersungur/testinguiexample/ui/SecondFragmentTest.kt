12101866bb91dcfe3f5656e6d65b825a93e18a1a
omers <omersungur615@gmail.com>
2023-05-29 18:20:04 +0300

    added test cases about second fragment

--- app/src/androidTest/java/com/omersungur/testinguiexample/ui/SecondFragmentTest.kt
+++ app/src/androidTest/java/com/omersungur/testinguiexample/ui/SecondFragmentTest.kt
@@ -1,12 +1,29 @@
 package com.omersungur.testinguiexample.ui
 
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
+import androidx.fragment.app.testing.launchFragmentInContainer
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito
 
 @RunWith(AndroidJUnit4::class)
-@LargeTest
 class SecondFragmentTest {
 
+    @Test
+    fun pressBackButton_popBackStack() {
+        val navController = Mockito.mock(NavController::class.java)
+
+        val scenario = launchFragmentInContainer<SecondFragment>()
+        scenario.onFragment { fragment ->
+            Navigation.setViewNavController(fragment.requireView(), navController)
+        }
+
+        Espresso.pressBack()
+
+        Mockito.verify(navController).popBackStack()
+    }
 
 }
